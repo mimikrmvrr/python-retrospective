a4dcb1eb25449c3f0fb5153079209c00959226ee
Mariya Miteva <mimi_9204@abv.bg>
2013-04-29 12:25:24 +0300

    better solution
    combine two vectors in a dict, using range, and shorter
    what_is_my_sign(day, month) function

--- task1/solution.py
+++ task1/solution.py
@@ -1,11 +1,21 @@
-signs_dates = [19, 18, 20, 20, 20, 20, 21, 22, 22, 22, 21, 21]
-signs_names = ["Козирог", "Водолей", "Риби", "Овен", "Телец", "Близнаци",
-               "Рак", "Лъв", "Дева", "Везни", "Скорпион", "Стрелец"]
+SIGNS = {
+    range(101, 120): "Козирог",
+    range(120, 219): "Водолей",
+    range(219, 321): "Риби",
+    range(321, 421): "Овен",
+    range(421, 521): "Телец",
+    range(521, 621): "Близнаци",
+    range(631, 722): "Рак",
+    range(722, 823): "Лъв",
+    range(823, 923): "Дева",
+    range(923, 1023): "Везни",
+    range(1023, 1122): "Скорпион",
+    range(1122, 1222): "Стрелец",
+    range(1222, 1232): "Козирог"}
 
 
-def what_is_my_sign(day=1, month=1):
-    if month == 12:
-        month = 0
-    if day > signs_dates[month-1]:
-        month += 1
-    return signs_names[month-1]
+def what_is_my_sign(day, month):
+    date = month * 100 + day
+    for range_sign in SIGNS.keys():
+        if date in range_sign:
+            return SIGNS[range_sign]
