b2bb18475d4e96fa12be34031d75ab3f59fd3214
Mariya Miteva <mimi_9204@abv.bg>
2013-04-29 12:55:35 +0300

    better solution
    better style (remove extra brackets and etc.)
    use OrderedDict in cache(func, cache-size) function

--- task2/solution.py
+++ task2/solution.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+
+
 def groupby(func, seq):
     result = {}
     for value in seq:
@@ -6,7 +9,7 @@ def groupby(func, seq):
 
 
 def comp(function1, function2):
-    return (lambda arg: function1(function2(arg)))
+    return lambda arg: function1(function2(arg))
 
 
 def iterate(func):
@@ -19,27 +22,19 @@ def iterate(func):
 def zip_with(func, *iterables):
     if len(iterables) < 1:
         return []
-    else:
-        return (func(*elements) for elements in zip(*iterables))
+    return (func(*elements) for elements in zip(*iterables))
 
 
 def cache(func, cache_size):
     if cache_size == 0:
         return func
-    else:
-        cache_results = {}
-        cache_order = []
-        front_index = 0
+    cached_results = OrderedDict()
 
-        def func_cached(*args):
-            nonlocal cache_results, cache_order, front_index
-            if not args in cache_results:
-                result = func(*args)
-                cache_order.append(args)
-                cache_results[args] = result
-                if (len(cache_order) - front_index) > cache_size:
-                    del cache_results[cache_order[front_index]]
-                    front_index += 1
-            return cache_results[args]
+    def func_cached(*args):
+        if not args in cached_results:            
+            cached_results[args] = func(*args)
+            if len(cached_results) > cache_size:
+                cached_results.popitem(last=False)
+        return  cached_results[args]
 
     return func_cached
